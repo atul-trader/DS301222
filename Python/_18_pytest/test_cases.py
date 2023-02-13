6eb04bdd8019e32d1fc23bfc827e3bad23c295d7
Bharati <bharatiyadav14@gmail.com>
2023-02-13 21:23:21 +0530

    pdb | pytest | mini_project

---
+++ Python/_18_pytest/test_cases.py
@@ -0,0 +1,17 @@
+# Command to install pytest
+# pip install pytest
+
+from _1_pytest_demo import add
+
+def test_add_int():
+    assert add(2,3) == 5
+
+def test_add_str():
+    assert add("a","b") == "abc"
+
+class Test_Cases:
+    def test_add_int(self):
+        assert add(2,3) == 5
+
+    def test_add_str(self):
+        assert add("a","b") == "abc"
