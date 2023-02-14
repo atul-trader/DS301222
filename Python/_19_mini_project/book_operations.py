85a3bbb98570e75015b6e0ab4508ed162bc40a6b
Bharati <bharatiyadav14@gmail.com>
2023-02-14 21:46:56 +0530

    mini-project

--- Python/_19_mini_project/book_operations.py
+++ Python/_19_mini_project/book_operations.py
@@ -1,9 +1,9 @@
 from book import Book
 
 class Operation:
+    booklist = []
 
     def addBook(self):
-        self.booklist = []
         ID = int(input("Enter Book Id : "))
         name = input("Enter Book Name : ")
         edition = input("Enter Book Edition Name : ")
@@ -14,4 +14,49 @@ class Operation:
         print("Book Added Successfully!")
 
     def viewBook(self):
-        pass
+        for i in self.booklist:
+            print(i,"\n")
+
+    def deleteBook(self):
+        ID = int(input("Enter Book Id which you want to delete : "))
+        for books in self.booklist:
+            if books.get_id() == ID:
+                self.booklist.remove(books)
+                print("Book Successfully Deleted!")
+                break
+        else:
+            print("Book Not Found")
+
+    def searchBookByID(self):
+        ID = int(input("Enter Book ID: "))
+        for books in self.booklist:
+            if books.get_id() == ID:
+                print(books)
+                break
+        else:
+            print("Book Not Found")   
+
+    def searchBookByName(self):
+        name = input("Enter name of book you want to search : ")
+        for books in self.booklist:
+            if books.get_name()==name:
+                print(books)       
+        else:
+            print("Book not found")
+
+    def updateBook(self):
+        ID=int(input("Enter the ID of book you want to search : "))
+        for books in self.booklist:
+            if books.get_id()==ID:
+                name = input("Enter Book Name : ")
+                edition = input("Enter Book Edition Name : ")
+                publisher = input("Enter Book Publisher Name : ")
+                price = float(input("Enter Book Price : "))
+                books.set_name(name)
+                books.set_edition(edition)
+                books.set_publisher(publisher)
+                books.set_price(price)        
+        else:
+            print("book not found")
+
+            
--- Python/_19_mini_project/main.py
+++ Python/_19_mini_project/main.py
@@ -9,12 +9,30 @@ class Main:
 
         if choice == 2:
             print("******View Book******")
+            operation.viewBook()
 
+        if choice == 3:
+            print("********Delete Book******")
+            operation.deleteBook()
+
+        if choice == 4:
+            print("********Seach Book By ID******")
+            operation.searchBookByID()
+
+        if choice == 5:
+            print("********Seach Book By Name******")
+            operation.searchBookByName()
+
+        if choice == 6:
+            print("********Update Book******")
+            operation.updateBook()
+
+         
 
 if __name__ == "__main__":
     
     operation = Operation()
     main = Main()
     while True:
-        choice = int(input("Enter \n1.Add Book \n2.View Book \n3.Delete Book : \n"))
+        choice = int(input("Enter \n1.Add Book \n2.View Book \n3.Delete Book \n4.Search Book By ID \n5.Search Book By Name \n6.Update Book : \n"))
         main.execute(choice=choice,operation=operation)
